f017a6ac7d44a7857a2acab44d3b4793b0d29f0d
Frowingg <edbico0@gmail.com>
2022-06-13 23:18:19 +0200

    Palindroma

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,1 +1,20 @@
+const userWord = prompt('dammi una stringa');
+const word = userWord.replace(/\s+/g, '').toLowerCase();
+let newWord = '';
 
+function isPalindrome(word) {
+
+    for(let i = word.length -1; i>=0; i--) {
+        newWord += `${word[i]}`;
+    }
+    if (word === newWord) {
+        return 'è palidroma';
+    } else {
+        return 'non è palindroma';
+    }
+
+}
+
+
+const output = isPalindrome(word);
+console.log('output', output)
